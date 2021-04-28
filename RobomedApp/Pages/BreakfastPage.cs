9b60ede766b0032ee75a1367777472d4db4c8c2e
Daniel Horț <dnielhort@gmail.com>
2021-04-28 22:44:08 +0300

    initial commit

---
+++ RobomedApp/Pages/BreakfastPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobomedApp.Pages
+{
+    public partial class BreakfastPage
+    {
+        public string Title { get; }
+
+        public BreakfastPage()
+        {
+            Title = "Take your breakfast";
+        }
+    }
+}
